b0976e748ce92eea762f7f0dad0243e373abf597
Abdou Drame <abdoudrame0418@gmail.com>
2026-04-28 13:46:32 +0200

    feat : navigation vers commandes + bouton Commander

--- web/components/Header.js
+++ web/components/Header.js
@@ -4,11 +4,19 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header-content">
+        {/* Le logo qui ramène à l'accueil */}
         <Link href="/" className="header-logo">
           🍛 Terranga<span>Food</span>
         </Link>
+
+        {/* La navigation principale */}
         <nav className="header-nav">
           <Link href="/">Restaurants</Link>
+          
+          {/* Nouveau lien ajouté ici */}
+          <Link href="/mes-commandes">
+            Mes commandes
+          </Link>
         </nav>
       </div>
     </header>
